223af86089fce1cde7ee47c6c28d6079692f5bb1
Prathamesh <prathameshaj2004@gmail.com>
2024-08-28 19:33:37 +0530

    Changes

---
+++ Top 50 Coding Interview Questions/15-Write a code to replace each element in an array by its rank in the array/replaceeachelementinanarraybyitsrankinthearray.py
@@ -0,0 +1,15 @@
+def changeArr(input1):
+
+    newArray = input1.copy()
+    newArray.sort()
+
+    for i in range(len(input1)):
+        for j in range(len(newArray)):
+            if input1[i] == newArray[j]:
+                input1[i] = j + 1;
+                break;
+#Driver code
+arr = [100 , 2 ,70, 12 ,90]
+changeArr(arr)
+#print the array elements
+print(arr)
